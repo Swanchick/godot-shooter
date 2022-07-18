4fdeb4b4f1d546e638d9a89a98988c43f33ea8f1
Swanchick <61909965+Swanchick@users.noreply.github.com>
2022-07-18 18:53:26 +0300

    Update Inventory.gd

--- Script/Player/Inventory.gd
+++ Script/Player/Inventory.gd
@@ -1,3 +1,15 @@
 extends Spatial
 
-var test = "Hello World"
+var weapons: Array = []
+
+
+func can_add(weapon: String) -> bool:
+	return weapon in weapons
+
+
+func add_weapon(weapon: String):
+	weapons.append(weapon)
+
+func _input(event):
+	if event.as_text().is_valid_integer():
+		print(event.as_text())
